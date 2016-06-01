eb1a1a44f7e81c9f6241e3435a0b75a370d67a8f
patrickswijgman <petrik09@live.nl>
2016-06-01 20:49:39 +0200

    header

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,5 +1,11 @@
 @extends('layout')
 
+@section('header')
+
+    <link href="css/calendar-custom.css" rel="stylesheet">
+
+@stop
+
 @section('content')
 
     <h1><div class="dateTitle"></div></h1>
@@ -11,13 +17,13 @@
             <button class="btn btn-primary" data-calendar-nav="next">Volgende >></button>
         </div>
         <div class="btn-group">
-            <button class="btn btn-warning" data-calendar-view="year">Jaar</button>
-            <button class="btn btn-warning active" data-calendar-view="month">Maand</button>
-            <button class="btn btn-warning" data-calendar-view="week">Week</button>
-            <button class="btn btn-warning" data-calendar-view="day">Dag</button>
+            <button class="btn btn-info" data-calendar-view="year">Jaar</button>
+            <button class="btn btn-info active" data-calendar-view="month">Maand</button>
+            <button class="btn btn-info" data-calendar-view="week">Week</button>
+            <button class="btn btn-info" data-calendar-view="day">Dag</button>
         </div>
     </div>
-    <br/><br/>
+    <br/>
 
     {!! $calender->render() !!}
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,9 +13,6 @@
     <!-- Custom -->
     <link href="css/bootstrap-custom.css" rel="stylesheet">
 
-    <!-- Other -->
-    <link href="css/calendar-custom.css" rel="stylesheet">
-
     @yield('header')
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
