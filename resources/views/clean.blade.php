aa6b78d1c676d5438ed6e33b1cb7db49c21fd0bd
thijs <12345>
2016-06-02 14:28:02 +0200

    links now uses laravels functions

--- resources/views/clean.blade.php
+++ resources/views/clean.blade.php
@@ -8,18 +8,18 @@
     <title>Pinnalot</title>
 
     <!-- Bootstrap -->
-    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    {{ Html::style("css/bootstrap.min.css") }}
 
     <!-- Custom -->
-    <link href="/css/bootstrap-custom.css" rel="stylesheet">
+    {{ Html::style('css/bootstrap-custom.css') }}
 
     @yield('header')
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    {{ Html::script('https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js') }}
+    {{ Html::script("https://oss.maxcdn.com/respond/1.4.2/respond.min.js") }}
     <![endif]-->
 </head>
 <body>
@@ -32,9 +32,9 @@
 </div> <!-- Div opens in header -->
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+{{ Html::script("https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js") }}
 <!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="/js/bootstrap.min.js"></script>
+{{ Html::script('js/bootstrap.min.js') }}
 
 @yield('footer')
 
