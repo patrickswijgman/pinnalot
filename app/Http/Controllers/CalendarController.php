fc9df93cab6895d96f5370d4da34c57a09223773
patrickswijgman <petrik09@live.nl>
2016-06-02 11:33:13 +0200

    Migration

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -10,15 +10,8 @@ use App\Elements\BootstrapCalendar;
 class CalendarController extends Controller
 {
     function view(){
-        $events = array(
-            'id' => 1,
-            'title' => 'event 1',
-            'url' => 'event/1',
-            'class' => 'event-important',
-            'start' => round(microtime(true) * 1000),
-            'end' => round(microtime(true) * 1000)
-        );
-        $json = '[' . json_encode($events) . ']';
+
+
         return view('/calendar', ['events' => $json]);
     }
 }
