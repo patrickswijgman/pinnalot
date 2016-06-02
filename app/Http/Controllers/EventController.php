292ef1592bf893c9ead1b325cb0e0d6cc78c37b0
patrickswijgman <petrik09@live.nl>
2016-06-02 14:06:34 +0200

    defensive programming

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,13 +9,9 @@ use App\Http\Requests;
 
 class EventController extends Controller
 {
-    function show($id){
 
         /** @var Event $event */
         $event = Event::find($id);
-        $event->start = Event::convertTimestampToDateTime($event->start);
-        $event->end = Event::convertTimestampToDateTime($event->end);
 
-        return view('/event', ['event' => $event]);
     }
 }
