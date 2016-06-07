81855860cf82267079424de56c8c05c6f8bfccfb
patrickswijgman <petrik09@live.nl>
2016-06-07 19:46:12 +0200

    ugly popup

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -35,6 +35,20 @@
     </button>
     <div id='calendar' ></div>
     <div id="source" data-events="{{$events}}" ></div>
+
+    <button type="button" class="mdl-button show-modal">Show Modal</button>
+    <dialog class="mdl-dialog">
+        <div class="mdl-dialog__content">
+            <p>
+                Allow this site to collect usage data to improve your experience?
+            </p>
+        </div>
+        <div class="mdl-dialog__actions mdl-dialog__actions--full-width">
+            <button type="button" class="mdl-button">Agree</button>
+            <button type="button" class="mdl-button close">Disagree</button>
+        </div>
+    </dialog>
+
 @stop
 
 @section('footer')
@@ -47,9 +61,27 @@
                     left: '',
                     center: 'title',
                     right: ''
+                },
+                eventClick: function(event) {
+                    if (event.url) {
+                        var url = '{{ url("event/:id") }}';
+                        url = url.replace(':id', event.id);
+                        windowpop(url, 400, $(window).height());
+                        return false;
+                    }
                 }
-            })
+            });
         });
+
+        function windowpop(url, width, height) {
+            var leftPosition, topPosition;
+            //Allow for borders.
+            leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+            //Allow for title and status bars.
+            topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+            //Open the window.
+            window.open(url, "Window2", "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no");
+        }
     </script>
 
 @stop
