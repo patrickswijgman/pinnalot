e94968fde5e5e60f55d7cd45d0c9b645a13485a5
patrickswijgman <petrik09@live.nl>
2016-06-08 12:15:31 +0200

    month names

--- public/js/init_fullcalendar.js
+++ public/js/init_fullcalendar.js
@@ -12,7 +12,10 @@ function convertISOtoDateTime(timeString) {
     var dateTime = timeString.split("T");
     var date = dateTime[0].split("-");
     var time = dateTime[1].split(":");
-    return date[2] + ' ' + date[1] + ' ' + date[0] + '<br/>' + time[0] + ':' + time[1];
+    //TODO make less dirty
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+        'August', 'September', 'October', 'November', 'December'];
+    return date[2] + ' ' + months[parseInt(date[1])-1] + ' ' + date[0] + '<br/>' + time[0] + ':' + time[1];
 }
 
 $(document).ready(function() {
