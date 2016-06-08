24ce5d0ac711cf463e565628d715989f1cbdc898
patrickswijgman <petrik09@live.nl>
2016-06-08 12:31:51 +0200

    js to footer

--- public/js/init_fullcalendar.js
+++ public/js/init_fullcalendar.js
@@ -8,16 +8,6 @@ if (dialog !== null) {
     });
 }
 
-function convertISOtoDateTime(timeString) {
-    var dateTime = timeString.split("T");
-    var date = dateTime[0].split("-");
-    var time = dateTime[1].split(":");
-    //TODO make less dirty
-    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-        'August', 'September', 'October', 'November', 'December'];
-    return date[2] + ' ' + months[parseInt(date[1])-1] + ' ' + date[0] + '<br/>' + time[0] + ':' + time[1];
-}
-
 $(document).ready(function() {
     $('#calendar').fullCalendar({
         events: $('#source').data("events"),
