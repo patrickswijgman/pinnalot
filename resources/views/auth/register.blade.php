9b04b14c8869cb119d6dcd2001aefe83e8b9c9da
patrickswijgman <petrik09@live.nl>
2016-06-10 14:49:23 +0200

    register fix voor Iris

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,6 @@
-@extends('layout')
+@extends('clean')
 
-@section('content')
+@section('clean-content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
