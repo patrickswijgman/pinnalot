1e93c7ca09c652364cb92e80103385a81dc968bc
thijs <thijsdehaan71@hotmail.com>
2016-06-16 12:14:49 +0200

    fix seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,11 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      *
@@ -11,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+        $this->call(GroupTypeSeeder::class);
+        $this->call(MdlColorsSeeder::class);
+        Model::reguard();
     }
 }
