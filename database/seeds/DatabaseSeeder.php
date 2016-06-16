cb80751044ba7c694b3addf03fda2ef994959be7
irismeijer <i.e.meijer@hotmail.com>
2016-06-16 14:45:11 +0200

    Seeder voor users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+<<<<<<< Updated upstream
         // $this->call(UsersTableSeeder::class);
+=======
+        Model::unguard();
+        $this->call(GroupTypeSeeder::class);
+        $this->call(MdlColorsSeeder::class);
+        $this->call(SettingsDefaultSeeder::class);
+        $this->call(UsersSeeder::class);
+        Model::reguard();
+>>>>>>> Stashed changes
     }
 }
---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\UserData;
+use App\User;
+use Illuminate\Database\Seeder;
+
+use Faker\Factory as Faker;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach (range(1,10) as $index) {
+            $name = $faker->name;
+            $userData = UserData::create(['firstname' => $name]);
+            DB::table('users')->insert([
+                'name' => $name,
+                'email' => $faker->email,
+                'password' => bcrypt('password'),
+                'userData' => $userData->id
+            ]);
+        }
+    }
+}
