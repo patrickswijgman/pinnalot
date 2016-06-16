a74cd8e87e885883302868f807f15225e2abdc06
patrickswijgman <petrik09@live.nl>
2016-06-16 13:09:28 +0200

    yo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
         $this->call(GroupTypeSeeder::class);
         $this->call(MdlColorsSeeder::class);
+        $this->call(SettingsDefaultSeeder::class);
         Model::reguard();
     }
 }
