8326a9d6d2cb80f773d2738b4f60d0c80331c62a
Frank de Voogd <frankdevoogd@gmail.com>
2016-06-21 14:22:59 +0200

    Update
    Search on email
    Check that if user already in group, not displayed

--- resources/views/group_person_add.blade.php
+++ resources/views/group_person_add.blade.php
@@ -2,34 +2,32 @@
 
 @section('content')
 
-  @if ($users->isEmpty())
-   No users found.
-  @else
-    {{ Form::open(array('url' => url('/group/'.$group->id.'/add'))) }}
-    {{ csrf_field() }}
+  @if ($member_of == null)
+      {{ Form::open(array('url' => url('/group/'.$group->id.'/add'))) }}
+      {{ csrf_field() }}
 
-    {{ MdlForm::showAllErrors($errors) }}
+      {{ MdlForm::showAllErrors($errors) }}
 
-    <div class = 'candidates-list'>
-      <ul class="demo-list-control mdl-list">
-      @foreach($users as $candidate)
-          <li class="mdl-list__item">
+      <div class = 'candidates-list'>
+          <ul class="demo-list-control mdl-list">
+                  <li class="mdl-list__item">
             <span class="mdl-list__item-primary-content">
               <i class="material-icons  mdl-list__item-avatar">person</i>
-              {{ $candidate->firstname . ' ' . $candidate->lastname }}
+                {{ $users->firstname . ' ' . $users->lastname }}
             </span>
             <span class="mdl-list__item-secondary-action">
               <label class="list-radio mdl-radio mdl-js-radio mdl-js-ripple-effect" for="list-option-1">
-                <input type="radio" id="list-option-1" class="mdl-radio__button" name="candidate_radio" value="{{$candidate->id}}" checked />
+                <input type="radio" id="list-option-1" class="mdl-radio__button" name="candidate_radio" value="{{$users->id}}" checked />
               </label>
             </span>
-          </li>
-      @endforeach
-      </ul>
-    </div>
+                  </li>
+          </ul>
+      </div>
 
-    {{MdlForm::submit('Add')}}
-    {{ Form::close() }}
+      {{MdlForm::submit('Add')}}
+      {{ Form::close() }}
+  @else
+    No users found.
   @endif
 
 @endsection
